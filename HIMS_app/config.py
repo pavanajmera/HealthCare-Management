803f9377eebe82ef24b58b7662408a507229c401
Ajmera Pavan Kumar <67977999+pavankumar0801@users.noreply.github.com>
2020-07-08 01:17:56 +0530

    Add files via upload

---
+++ HIMS_app/config.py
@@ -0,0 +1,6 @@
+# configuration file containing confidential credentials
+
+password = '15081812'       # user authentication password
+database_name = 'database_v2_2'     # current database name
+edit_mode_password = 'allow_edit'       # edit mode password
+dr_mls_access_code = 'access_auth'      # doctor/medical lab scientist access code
